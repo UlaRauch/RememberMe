eb9f95b5935756f4ef9d8e6b172d0ddd16a49eb3
AnnaLeitner <anna.leitner@stud.fh-campuswien.ac.at>
2022-05-13 15:27:11 +0200

    Widgets Anpassung

--- app/src/main/java/com/example/rememberme/widgets/RememberWidgets.kt
+++ app/src/main/java/com/example/rememberme/widgets/RememberWidgets.kt
@@ -6,9 +6,14 @@ import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.HorizontalAlignmentLine
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.rememberme.models.Reminder
 import com.example.rememberme.screens.reminderFilter
@@ -23,38 +28,46 @@ fun RememberRow(
         modifier = Modifier
             .padding(4.dp)
             .fillMaxWidth()
+            .heightIn(min = 0.dp)
             .clickable {onItemClick(reminder.id)},
         shape = RoundedCornerShape(corner = CornerSize(16.dp)),
         elevation = 6.dp
     ) {
-        Row() {
-            Column {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Surface(
+                modifier = Modifier
+                    .padding(12.dp)
+                    //.size(100.dp),
 
-                Text(
-                    text = reminder.title,
-                    style = MaterialTheme.typography.h6
-                )
-                Row() {
+                ) {
+                Column() {
+
                     Text(
-                        text = "Date: ${reminder.day}.",
-                        style = MaterialTheme.typography.caption
+                        text = reminder.title,
+                        style = MaterialTheme.typography.h6,
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
                     )
+                    Row() {
+                        Text(
+                            text = "Date: ${reminder.day}.",
+                            style = MaterialTheme.typography.caption
+                        )
+                        Text(
+                            text = "${reminder.month}.",
+                            style = MaterialTheme.typography.caption
+                        )
+                        Text(
+                            text = "${reminder.year}",
+                            style = MaterialTheme.typography.caption
+                        )
+                    }
+
                     Text(
-                        text = "${reminder.month}.",
-                        style = MaterialTheme.typography.caption
-                    )
-                    Text(
-                        text = "${reminder.year}",
+                        text = "Text: ${reminder.text}",
                         style = MaterialTheme.typography.caption
                     )
                 }
-
-                Text(
-                    text = "Text: ${reminder.text}",
-                    style = MaterialTheme.typography.caption
-                )
-                }
-
+            }
         }
     }
 }
