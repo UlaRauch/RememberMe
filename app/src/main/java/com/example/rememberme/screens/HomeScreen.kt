8a9cda67aa62cab3472968b4fb30858f2498f06c
UlaRauch <71543978+UlaRauch@users.noreply.github.com>
2022-05-24 14:09:48 +0200

    added deletebutton (reminders disappear on next reload)

--- app/src/main/java/com/example/rememberme/screens/HomeScreen.kt
+++ app/src/main/java/com/example/rememberme/screens/HomeScreen.kt
@@ -1,17 +1,24 @@
 package com.example.rememberme.screens
 
 import android.util.Log
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.*
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.rememberme.models.Reminder
@@ -31,10 +38,21 @@ fun HomeScreen(
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text(text = "RememberMe") },
+                //title = "RememberMe",
+                //title = { Text(text = "RememberMe") },
                 backgroundColor = Purple600,
                 contentColor = Color.White
-            )
+            ) {
+                Row() {
+                    Text(text = "RememberMe", textAlign = TextAlign.Center)
+                    Icon(imageVector = Icons.Default.Delete,
+                        contentDescription = "delete all reminders",
+                        modifier = Modifier.clickable {
+                            viewModel.deleteAll()
+                        })
+
+                }
+            }
         },
         floatingActionButton = {
             FloatingActionButton(
