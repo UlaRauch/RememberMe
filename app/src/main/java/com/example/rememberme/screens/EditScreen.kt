825893ae31f5771a621baadc292ab2e1c093d09b
AnnaLeitner <anna.leitner@stud.fh-campuswien.ac.at>
2022-06-08 13:24:27 +0200

    text, title, date and time of the selected reminder is displayed NOT DONE: modified reminder is displayed in the details screen PROBLEM: as soon as reminder is adjusted, reminder is null

--- app/src/main/java/com/example/rememberme/screens/EditScreen.kt
+++ app/src/main/java/com/example/rememberme/screens/EditScreen.kt
@@ -4,8 +4,8 @@ import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import android.content.Context
 import android.icu.util.Calendar
+import android.util.Log
 import android.widget.DatePicker
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -13,17 +13,13 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.runtime.*
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.example.rememberme.models.Reminder
-import com.example.rememberme.navigation.RememberScreens
 import com.example.rememberme.ui.theme.Green600
 import com.example.rememberme.ui.theme.Purple600
-import com.example.rememberme.viewmodels.DetailRememberViewModel
 import com.example.rememberme.viewmodels.EditRememberViewModel
 import java.util.*
 
@@ -35,45 +31,31 @@ fun EditScreen(
     reminderID: Long = 1,
     context: Context
 ) {
+
     editViewModel.getReminderbyID(reminderID = reminderID)
     val reminder = editViewModel.reminder
+
+    Log.d("editscreen", "you're in the editscreen")
     //val reminder = Reminder(title = "next", d = 1, m = 1, y = 2023, h = 22, min = 0, text = "")
     Scaffold(
         topBar = {
-            TopAppBar(backgroundColor = Purple600, contentColor = Color.White
-            ) {
-                Row(){
-                    Icon(
-                        imageVector = Icons.Default.ArrowBack,
-                        contentDescription = "Arrow Back",
-                        modifier = Modifier.clickable {
-                            navController.popBackStack()
-                        }
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "Edit ${reminder.value?.title}",
+                        color = Color.White
                     )
-
-                    Text( text = "New Reminder",
-                        modifier = Modifier.absolutePadding(110.dp, 0.dp,110.dp,0.dp))
-
-                }
-
-            }
-        },
-        floatingActionButton = {
+                },
+                navigationIcon = {
+                    IconButton(onClick = {navController.popBackStack()}) {
+                        Icon(Icons.Default.ArrowBack,"Arrow Back")
+                    }
+                },
+                backgroundColor = Purple600,
+                contentColor = Color.White
+            )},floatingActionButton = {
             FloatingActionButton(onClick = {
-               /* addViewModel.addReminder()
-
-                val delayInSeconds = getDelayInSeconds(
-                    addViewModel.reminder.value!!.y,
-                    addViewModel.reminder.value!!.m -1, //months are represented as index https://developer.android.com/reference/java/util/Date.html#Date%28int,%20int,%20int,%20int,%20int,%20int%29
-                    addViewModel.reminder.value!!.d,
-                    addViewModel.reminder.value!!.h,
-                    addViewModel.reminder.value!!.min,
-                )
-                createWorkRequest(
-                    addViewModel.reminder.value!!.title,
-                    addViewModel.reminder.value!!.text,
-                    delayInSeconds,
-                    context)*/
+                reminder.value?.let { editViewModel.updateReminder(reminder = it) }
                 navController.popBackStack()
             },
                 backgroundColor = Green600,
@@ -88,28 +70,46 @@ fun EditScreen(
                     tint = Color.White,
                     modifier = Modifier.size(30.dp)
                 )
-                //  }
-
             }
-        }
-    ){
-        EditReminderCard(editViewModel = editViewModel, context = context)
-    //ReminderCard(addViewModel = addViewModel, context = context)
-    }
+        },
+        content = {
 
+            EditReminderCard(editViewModel = editViewModel, context =  context)
+             //ReminderCard(addViewModel = addViewModel, context = context)
+        })
 }
 
 @Composable
-fun EditReminderCard(editViewModel: EditRememberViewModel, context: Context) {
-    var text by remember { mutableStateOf("") }
-    // add more properties if you need
-    var date by remember { mutableStateOf("") }
+fun EditReminderCard(
+    editViewModel: EditRememberViewModel,
+    reminderID: Long = 1,
+    context: Context,
+) {
+    editViewModel.getReminderbyID(reminderID = reminderID)
+    val reminder = editViewModel.reminder //to access text, title, date or time of the selected reminder
+    Log.d("EditReminderCard", "EditReminderCard called")
+    //vars text, title
+    var text by remember { mutableStateOf("${reminder.value?.text}") }
     var title by remember { mutableStateOf("") }
 
+    //vars for date
     var y: Int
     var m: Int
     var d: Int
     val nowDate = Calendar.getInstance()
+
+    //vals and vars for Time
+    // Fetching local context
+    val mContext = LocalContext.current
+
+    // Declaring and initializing a calendar
+    val nowTime = Calendar.getInstance() //cal means Calendar
+    var h = nowTime[Calendar.HOUR_OF_DAY]
+    var min = nowTime[Calendar.MINUTE]
+
+    // Value for storing time as a string
+    val mTime = remember { mutableStateOf("") }
+
 
     //Title
     OutlinedTextField(
@@ -120,12 +120,12 @@ fun EditReminderCard(editViewModel: EditRememberViewModel, context: Context) {
         // onValueChange = {value -> addViewModel.setText(value)}, // immer wenn ich text änder dann ändert sich das im reminderobjekt aktuallisiert
         onValueChange = { value ->
             title = value    // update text value inside field
-        //    editViewModel.setTitle(value) // update value in viewmodel
+           editViewModel.setTitle(value) // update value in viewmodel
         },
-        label = { Text(text = "Title of your Reminder") },
-        placeholder = { Text(text = "Enter Title") },
+        label = { Text(text = "${reminder.value?.title}") },
+        placeholder = { Text(text = "Edit Title") },
         modifier = Modifier
-            .padding(20.dp,30.dp)
+            .padding(20.dp, 30.dp)
             .fillMaxWidth()
     )
 
@@ -143,7 +143,7 @@ fun EditReminderCard(editViewModel: EditRememberViewModel, context: Context) {
             y = year
             m = month+1 //months are represented as index https://developer.android.com/reference/java/util/Date.html#Date%28int,%20int,%20int,%20int,%20int,%20int%29
             d = dayOfMonth
-         //   editViewModel.setDate(d = d,m = m, y = y)
+            editViewModel.setDate(d = d,m = m, y = y)
             //date = "$dayOfMonth/$month/$year"
         }, y, m, d
     )
@@ -151,14 +151,14 @@ fun EditReminderCard(editViewModel: EditRememberViewModel, context: Context) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(20.dp,110.dp),
+            .padding(20.dp, 110.dp),
         verticalArrangement = Arrangement.Center,
         // horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Button(onClick = {
             datePickerDialog.show()
         }, modifier = Modifier.fillMaxWidth()) {
-            Text(text = "Open Date Picker")
+            Text(text = "${reminder.value?.d}.${reminder.value?.m}.${reminder.value?.y}")
         }
         Spacer(modifier = Modifier.size(16.dp))
 
@@ -167,17 +167,6 @@ fun EditReminderCard(editViewModel: EditRememberViewModel, context: Context) {
     }
 
     //Time
-    // Fetching local context
-    val mContext = LocalContext.current
-
-    // Declaring and initializing a calendar
-    val nowTime = Calendar.getInstance() //cal means Calendar
-    var h = nowTime[Calendar.HOUR_OF_DAY]
-    var min = nowTime[Calendar.MINUTE]
-
-    // Value for storing time as a string
-    val mTime = remember { mutableStateOf("") }
-
     // Creating a TimePicker dialog
     val mTimePickerDialog = TimePickerDialog(
         mContext,
@@ -186,44 +175,30 @@ fun EditReminderCard(editViewModel: EditRememberViewModel, context: Context) {
             calTime.set(mHour,mMinute)
             h = mHour
             min = mMinute
-         //   editViewModel.setTime(h = h, min = min)
+            editViewModel.setTime(h = h, min = min)
         }, h, min, false
     )
 
-/*
-    { _: DatePicker, year: Int, month: Int, dayOfMonth: Int ->
-        val cal = Calendar.getInstance()
-        cal.set(year, month, dayOfMonth)
-        y = year
-        m = month+1 //months are represented as index https://developer.android.com/reference/java/util/Date.html#Date%28int,%20int,%20int,%20int,%20int,%20int%29
-        d = dayOfMonth
-        addViewModel.setDate(d = d,m = m, y = y)
-        //date = "$dayOfMonth/$month/$year"
-    }, y, m, d
-
- */
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(20.dp,155.dp),
+            .padding(20.dp, 155.dp),
         verticalArrangement = Arrangement.Center,
         // horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Button(onClick = {
             mTimePickerDialog.show()
         }, modifier = Modifier.fillMaxWidth()) {
-            Text(text = "Open Time Picker")
+            Text(text = "${reminder.value?.h}:${reminder.value?.min}")
         }
         Spacer(modifier = Modifier.size(16.dp))
 
-        //addViewModel.setDate(d,m,y) // gives the date of day not selected date but i dunno how
-        //Text(text = "Selected date: ${mDay.value}.${mMonth.value}.${mYear.value}") //--> date.value is teh selected date
     }
 
 
     //Text
     /* von leons Branch*/
-    val reminder: Reminder? by editViewModel.reminder.observeAsState(null)
+   // val reminder: Reminder? by editViewModel.reminder.observeAsState(null)
     OutlinedTextField(
         //value = if (reminder != null) reminder!!.text else "", //schaut is reminder nicht null wenns da is dann wird der vom viewmodel angezeigt
         value = text,
@@ -232,10 +207,10 @@ fun EditReminderCard(editViewModel: EditRememberViewModel, context: Context) {
         // onValueChange = {value -> addViewModel.setText(value)}, // immer wenn ich text änder dann ändert sich das im reminderobjekt aktuallisiert
         onValueChange = { value ->
             text = value    // update text value inside field
-           // editViewModel.setText(value) // update value in viewmodel
+            editViewModel.setText(value) // update value in viewmodel
         },
-        label = { Text(text = "Reminder") },
-        placeholder = { Text(text = "Enter Text") },
+        label = { Text(text = "${reminder.value?.text}") },
+        placeholder = { Text(text = "Edit Text") },
         modifier = Modifier
             .padding(20.dp, 200.dp)
             .fillMaxWidth()
--- app/src/main/java/com/example/rememberme/viewmodels/EditRememberViewModel.kt
+++ app/src/main/java/com/example/rememberme/viewmodels/EditRememberViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.rememberme.viewmodels
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -18,9 +19,31 @@ class EditRememberViewModel (
 
     fun updateReminder(reminder:Reminder) {
         viewModelScope.launch(Dispatchers.IO) {
-          //  _reminder.postValue(repository.filterReminder(id = reminderID)) //postvalue instead of value because of coroutine https://stackoverflow.com/questions/51299641/difference-of-setvalue-postvalue-in-mutablelivedata?rq=1
+            //reminder.let { // call block only if not null
+              //  if (it.text.isNotEmpty()) {  // add more "Pflichtfelder" here if necessary
+                    repository.updateReminder(reminder)//
+                    Log.d("ViewModel", "reminder updated")
+                //}
+            //}
         }
     }
+    fun setText(text: String) {
+        _reminder.value?.text = text //wenn string übergeben wird wird geschaut ob eh nicht null
+    }
+    fun setDate(d: Int, m: Int, y: Int) {
+        _reminder.value?.d = d //wenn string übergeben wird wird geschaut ob eh nicht null
+        _reminder.value?.m = m
+        _reminder.value?.y = y
+    }
+    fun setTime(h:Int, min: Int) {
+        _reminder.value?.h = h //wenn string übergeben wird wird geschaut ob eh nicht null
+        _reminder.value?.min = min
+
+    }
+    fun setTitle(title: String) {
+        _reminder.value?.title = title //wenn string übergeben wird wird geschaut ob eh nicht null
+    }
+
     fun getReminderbyID(reminderID: Long) {
         viewModelScope.launch(Dispatchers.IO) {
             _reminder.postValue(repository.filterReminder(id = reminderID)) //postvalue instead of value because of coroutine https://stackoverflow.com/questions/51299641/difference-of-setvalue-postvalue-in-mutablelivedata?rq=1
